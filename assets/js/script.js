39e24eb24f45da40b263388b14f7eb5e509d1807
Konstantin Pak <konstantin.pak.it@gmail.com>
2022-11-10 12:54:26 +0600

    adwawdw

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,9 @@
 'use strict'
 
 const toggleFunc = (elem) => { elem.classList.toggle('active'); }
-
-const menu_burger = document.querySelector('.header__mobile-menu');
-const side_menu = document.querySelector('.side-menu');
 const goTopBtn = document.querySelector('[data-go-top]');
 
 
-menu_burger.addEventListener('click', function() {
-  toggleFunc(menu_burger);
-  toggleFunc(side_menu);
-});
 
 window.addEventListener("scroll", function() {
   if(this.window.scrollY >= 500) {
