4ca5d3cffb53928d2bca90cc1a454335a5fc9be3
Konstantin Pak <konstantin.pak.it@gmail.com>
2022-11-10 12:40:00 +0600

    sldvisaoevjosiehs

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,18 @@ const toggleFunc = (elem) => { elem.classList.toggle('active'); }
 
 const menu_burger = document.querySelector('.header__mobile-menu');
 const side_menu = document.querySelector('.side-menu');
+const goTopBtn = document.querySelector('[data-go-top]');
+
 
 menu_burger.addEventListener('click', function() {
   toggleFunc(menu_burger);
   toggleFunc(side_menu);
+});
+
+window.addEventListener("scroll", function() {
+  if(this.window.scrollY >= 500) {
+    goTopBtn.classList.add("active");
+  } else {
+    goTopBtn.classList.remove("active");
+  }
 });
