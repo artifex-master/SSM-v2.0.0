9c9e6234717570447f09bf226a279469c02978f3
Mr Konstantin <konstantin.pak.it@gmail.com>
2022-11-11 21:32:51 +0600

    Yandex Matrika: completed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-'use strict'
+'use strict';
 
-const toggleFunc = (elem) => { elem.classList.toggle('active'); }
-const goTopBtn = document.querySelector('[data-go-top]');
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();
+for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-
-
-window.addEventListener("scroll", function() {
-  if(this.window.scrollY >= 500) {
-    goTopBtn.classList.add("active");
-  } else {
-    goTopBtn.classList.remove("active");
-  }
+ym(91135492, "init", {
+    clickmap:true,
+    trackLinks:true,
+    accurateTrackBounce:true,
+    webvisor:true
 });
